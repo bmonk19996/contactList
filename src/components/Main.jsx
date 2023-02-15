3c1345feec88243492f4907fc1aed1a2e9b1a00a
MonkComputer <bmonk1996@yahoo.com>
2023-02-15 12:12:32 -0600

    api fetch finished

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,23 @@
-import React, { useState } from "react";
-import dummyContacts from "../dummyData";
+import React, { useEffect, useState } from "react";
 import ContactList from "./ContactList";
 
 const Main = () => {
-  const [contacts, setContacts] = useState(dummyContacts);
+  const [contacts, setContacts] = useState([]);
+  async function getContacts(){
+    try{
+      const response = await fetch('http://jsonplace-univclone.herokuapp.com/users')
+      const myContacts = await response.json()
+      return myContacts
+    }catch(err){
+      console.log(err)
+    }
+  }
+useEffect(async ()=>{
+  setContacts(await getContacts())
+},[])
+
+
+
   return (
     <div id="main">
       <div id="navbar">
